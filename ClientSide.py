d71715640662c00cd212228e86cb9f2d96fdbe34
jbroberto <jbroberto@yahoo.com.br>
2015-08-20 09:32:30 -0300

    Incluida a regra de validacao da entrada

--- ClientSide.py
+++ ClientSide.py
@@ -1,16 +1,24 @@
 import socket
-import sys
+import re
 
 HOST, PORT = "localhost", 9999
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+def matches(pwd):
+    return re.match("[a-zA-Z+],[0-9+]", pwd) is not None
+
 try:
     # Connect to server and send data
     sock.connect((HOST, PORT))
     data = raw_input("type a message: ")
-    sock.sendall(data + "\n")
+    if matches(data):
+        sock.sendall(data + "\n")
+    else:
+        print "You typed an invalid sequence. Try again\n"
+        data = raw_input("type a message: ")
 
     # Receive data from the server and shut down
     received = sock.recv(1024)
